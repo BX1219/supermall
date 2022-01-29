60e662e5c45d220f02359030f0f9e8adfd1c8b30
BX1219 <tanbangxu@qq.com>
2022-01-29 22:34:59 +0800

    初始化项目'

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style>
+ @import url("./assets/css/base.css");
+</style>
